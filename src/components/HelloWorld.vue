e18688b1ca9b31737bfcd7d374507315048154bd
liyijie <LYJ682699@yeah.net>
2020-06-28 15:19:42 +0800

    master

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@
     </p>
     <h3>Installed CLI Plugins</h3>
     <div>第一次修改</div>
-   
+    <div>第二次修改</div>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
